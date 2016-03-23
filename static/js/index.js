6dca344e6d7d3039e44c7bf59666871b1e2fc7f4
Demon000 <demonsingur@gmail.com>
2016-03-23 11:18:44 +0200

    Text gets selected on button press

--- static/js/index.js
+++ static/js/index.js
@@ -8,5 +8,21 @@ function shorty()
 	{
 		console.log(err, res);
 		document.querySelector('#shortened').innerHTML = `${location.protocol}//${location.host}/go/${res.body.short}`;
+		select(document.querySelector('#shortened'));
 	});
+}
+function select(dom)
+{
+	if(document.selection)
+	{
+		var range = document.body.createTextRange();
+		range.moveToElementText(dom);
+		range.select();
+	}
+	else if(window.getSelection)
+	{
+		var range = document.createRange();
+		range.selectNode(dom);
+		window.getSelection().addRange(range);
+	}
 }
